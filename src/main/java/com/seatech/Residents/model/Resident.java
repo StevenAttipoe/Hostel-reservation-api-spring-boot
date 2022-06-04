3744dbb065594b32e55960dda9551ec6308352b0
Steven Attipoe <stevenattipoe7@gmail.com>
2022-06-04 10:03:23 +0000

    debug model

--- src/main/java/com/seatech/Residents/model/Resident.java
+++ src/main/java/com/seatech/Residents/model/Resident.java
@@ -5,7 +5,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Resident {
-    @Getter @Setter private long id;
+    private long id;
     private int resident_id;
     private String resident_name;
     private String email;
